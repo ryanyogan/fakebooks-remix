5a36dc4bc9cf39db6cb0eeac58893f82f323384a
Ryan Yogan <ryanyogan@gmail.com>
2022-07-19 18:53:32 -0500

    auto logout after no use

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,12 @@
-import type { LinksFunction, MetaFunction } from "@remix-run/node";
+import type {
+  LinksFunction,
+  LoaderFunction,
+  MetaFunction,
+} from "@remix-run/node";
+import { json } from "@remix-run/node";
+import type { ShouldReloadFunction } from "@remix-run/react";
+import { useFetcher, useLocation } from "@remix-run/react";
+import { useLoaderData } from "@remix-run/react";
 import {
   Links,
   LiveReload,
@@ -9,6 +17,11 @@ import {
 } from "@remix-run/react";
 
 import tailwindStyles from "./styles/tailwind.css";
+import reachDialogStylesheet from "@reach/dialog/styles.css";
+import { getUser } from "./utils/session.server";
+import { useCallback, useEffect, useRef, useState } from "react";
+import Dialog from "@reach/dialog";
+import { dangerButtonClasses, submitButtonClasses } from "./components";
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
@@ -18,9 +31,21 @@ export const meta: MetaFunction = () => ({
 
 export const links: LinksFunction = () => [
   { rel: "stylesheet", href: tailwindStyles },
+  { rel: "stylesheet", href: reachDialogStylesheet },
 ];
 
+type LoaderData = {
+  user: Awaited<ReturnType<typeof getUser>>;
+};
+
+export const loader: LoaderFunction = async ({ request }) => {
+  return json<LoaderData>({
+    user: await getUser(request),
+  });
+};
+
 export default function App() {
+  const { user } = useLoaderData() as LoaderData;
   return (
     <html lang="en" className="h-full">
       <head>
@@ -29,6 +54,7 @@ export default function App() {
       </head>
       <body className="h-full">
         <Outlet />
+        {user ? <LogoutTimer /> : null}
         <ScrollRestoration />
         <Scripts />
         <LiveReload />
@@ -36,3 +62,71 @@ export default function App() {
     </html>
   );
 }
+
+function LogoutTimer() {
+  const [status, setStatus] = useState<"idle" | "show-modal">("idle");
+  const location = useLocation();
+  const fetcher = useFetcher();
+  const logoutTime = 1000 * 60 * 60 * 24; // 2 days
+  const modalTime = logoutTime - 1000 * 60 * 2; // 2 minutes
+  const modalTimer = useRef<ReturnType<typeof setTimeout>>();
+  const logoutTimer = useRef<ReturnType<typeof setTimeout>>();
+
+  const logout = useCallback(() => {
+    fetcher.submit(
+      { redirectTo: location.pathname },
+      { method: "post", action: "/logout" }
+    );
+    setStatus("idle");
+  }, [fetcher, location.pathname]);
+
+  const cleanupTimers = useCallback(() => {
+    clearTimeout(modalTimer.current);
+    clearTimeout(logoutTimer.current);
+  }, []);
+
+  const resetTimers = useCallback(() => {
+    cleanupTimers();
+
+    modalTimer.current = setTimeout(() => {
+      setStatus("show-modal");
+    }, modalTime);
+    logoutTimer.current = setTimeout(logout, logoutTime);
+  }, [cleanupTimers, logout, logoutTime, modalTime]);
+
+  useEffect(() => resetTimers(), [resetTimers, location.key]);
+  useEffect(() => cleanupTimers, [cleanupTimers]);
+
+  function closeModal() {
+    setStatus("idle");
+    resetTimers();
+  }
+
+  return (
+    <Dialog
+      aria-label="Pending Logout Notification"
+      isOpen={status === "show-modal"}
+      onDismiss={closeModal}
+    >
+      <div>
+        <h1 className="mb-4 text-d-h3">Are you still there?</h1>
+        <p>
+          You are going to be logged out due to inactivity. Close the modal to
+          stay logged in.
+        </p>
+        <div className="h-8" />
+        <div className="flex items-end gap-8">
+          <button className={submitButtonClasses} onClick={closeModal}>
+            Remain Logged In
+          </button>
+          <button onClick={logout} className={dangerButtonClasses}>
+            Logout
+          </button>
+        </div>
+      </div>
+    </Dialog>
+  );
+}
+
+export const unstable_shouldReload: ShouldReloadFunction = ({ submission }) =>
+  submission?.action === "/logout" || submission?.action === "/login";
