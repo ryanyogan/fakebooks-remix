039eca6249074d1ed33c653acde5636557657036
Ryan Yogan <ryanyogan@gmail.com>
2022-07-19 12:19:50 -0500

    Initial Config

---
+++ app/routes/index.tsx
@@ -0,0 +1,7 @@
+export default function Index() {
+  return (
+    <div className="text-slate-900">
+      <h1 className="text-slate-900 font-bold text-d-p-lg">Welcome to Remix</h1>
+    </div>
+  );
+}
---
+++ remix.config.js
@@ -0,0 +1,9 @@
+/** @type {import('@remix-run/dev').AppConfig} */
+module.exports = {
+  ignoredRouteFiles: [
+    ".*",
+    "**/*.css",
+    "**/*.test.{js,jsx,ts,tsx}",
+    "README.md",
+  ],
+};
